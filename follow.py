507a6bec2ae5b12ca8880e8d7182d5e4a8921275
Mark R. Boyd <mark.boyd.engineer@gmail.com>
2017-01-16 21:26:42 -0700

    fix follow.py for active low push-button

--- follow.py
+++ follow.py
@@ -2,7 +2,7 @@
 # the following assumes that an LED is run from VCC to xio-p0 with its cathode in the latter
 # ie: pulling pin low turns LED on, setting high turns it off
 
-# it also assums a normally-open pushbutton is connected between vcc and xio-p1
+# it also assums a normally-open pushbutton is connected between ground and xio-p1
 # when the pushbutton is pressed the LED should light. when it is released the
 # LED should be extinguished
 
@@ -12,10 +12,18 @@ GPIO.setup("XIO-P0", GPIO.OUT)
 GPIO.setup("XIO-P1", GPIO.IN)
 
 GPIO.output("XIO-P0", GPIO.HIGH) # turn LED off
+isPressed = False
+
 
 while True:
-    if GPIO.input('XIO-P1'):
-        GPIO.output("XIO-P0", GPIO.LOW)
+    if isPressed:
+        if GPIO.input('XIO-P1') == 1 : # button was released since it has a pull-up resistor
+            isPressed = False
+            GPIO.output("XIO-P0", GPIO.HIGH) # turn LED off
+            print("released")
     else:
-        GPIO.output("XIO-P0", GPIO.HIGH)
+        if GPIO.input('XIO-P1') == 0 : # button was pressed since button pulls it low
+            isPressed = True
+            GPIO.output("XIO-P0", GPIO.LOW) # turn LED on
+            print("pressed")
     time.sleep(0.05)  # sleep 50 milliseconds before checking again
--- sense.py
+++ sense.py
@@ -1,7 +1,9 @@
 
-# the following assums a normally-open pushbutton is connected between vcc and xio-p1
-# sense once per second if the button is pressed (high) and output its state either
-# 'pressed' or 'not-pressed'
+# the following assums a normally-open pushbutton is connected between to xio-p1
+# if its other lead is connected to vcc this program always outputs 1's. If it is
+# connected to ground it outputs 1 when not pressed, and 0 when pressed. this tells
+# us that the pin has a pull-up resistor which means we need to pull it low to signal
+# some outside event like pushing a button.
 
 import time
 import CHIP_IO.GPIO as GPIO
