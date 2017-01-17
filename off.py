9fb72de0f4063e4171b4e070c1e67efa37b25844
Mark R. Boyd <mark.boyd.engineer@gmail.com>
2017-01-16 20:45:20 -0700

    add on and off

---
+++ off.py
@@ -0,0 +1,8 @@
+
+# the following assumes that an LED is run from VCC to xio-p0 with its cathode in the latter
+# ie: pulling pin low turns LED on, setting high turns it off
+
+import CHIP_IO.GPIO as GPIO
+GPIO.setup("XIO-P0", GPIO.OUT)
+GPIO.output("XIO-P0", GPIO.HIGH)
+
