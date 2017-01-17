d23e3c32f7e5adb2bcc970fb0202b437fb3ef500
Mark R. Boyd <mark.boyd.engineer@gmail.com>
2017-01-16 20:54:25 -0700

    one second blink

--- blink.py
+++ blink.py
@@ -1,5 +1,15 @@
 
+# the following assumes that an LED is run from VCC to xio-p0 with its cathode in the latter
+# ie: pulling pin low turns LED on, setting high turns it off
+
+import time
 import CHIP_IO.GPIO as GPIO
 GPIO.setup("XIO-P0", GPIO.OUT)
-GPIO.output("XIO-P0", GPIO.LOW)
 
+GPIO.output("XIO-P0", GPIO.HIGH)
+
+while True
+    GPIO.output("XIO-P0", GPIO.LOW)
+    time.sleep(1)
+    GPIO.output("XIO-P0", GPIO.HIGH)
+    time.sleep(1)
