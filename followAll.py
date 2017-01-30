95bbcf3e841a678bb4f51b19b33ee6b0c6c3505b
Mark R. Boyd <mark.boyd.engineer@gmail.com>
2017-01-29 22:00:04 -0700

    support all buttons pressed

--- followAll.py
+++ followAll.py
@@ -24,8 +24,7 @@ while True:
 
     time.sleep(0.05)  # sleep 50 milliseconds before checking again
     for btn in buttons:
-        print("checking: " + btn.color + " - on: " + str(btn.isLedOn()) ", pressed: "
-            + str(btn.isPressed()) + ", released: " + str(btn.isReleased()))
+        print("checking: " + btn.color + " - on: " + str(btn.isLedOn()) ", pressed: " + str(btn.isPressed()) + ", released: " + str(btn.isReleased()))
         if btn.isLedOn():
             if btn.isReleased():
                 btn.turnOff()
