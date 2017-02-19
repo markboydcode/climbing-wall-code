0e31d4c0e98f7fd636262e720ebc9a2a1825e9be
Mark R. Boyd <mark.boyd.engineer@gmail.com>
2017-02-19 16:19:11 -0700

    triPhase solicitation

--- Solicitations.py
+++ Solicitations.py
@@ -26,7 +26,7 @@ class Continuous():
         return False
 
 
-def blinkTimes(durationMillis, times)
+def blinkTimes(durationMillis, times):K
     # blink means off for half and on for half. Each of these halfs is a phase. There are twice as many phases as
     # there are flashes.
     phases = times * 2
